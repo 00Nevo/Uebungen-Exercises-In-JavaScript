491af2c989d3ba1e8e2cae2efbd043912df65b11
NEVO <albertoborgato9@gmail.com>
2025-03-28 13:04:09 +0100

    Create Ch1.js

---
+++ Ch1.js
@@ -0,0 +1,25 @@
+Building JavaScript Games
+Arjan Egges
+Cross-platform game programming emphasizing mobile platforms and touch-based interfaces for Phones, Tablets, and Desktop
+
+Introduction .................................................................................................................... xxvii
+■ Part I: Getting Started ................................................................................... 1
+■ Chapter 1: Programming ...................................................................................................3
+Computers and Programs
+Processor and Memory
+Programs
+The Early Days: Imperative Programming
+Procedural Programming: Imperative + Procedures
+Object-Oriented Programming: Procedural + Objects
+Web Applications
+Programming Games
+Developing Games
+Small Scale: Edit-Interpret-Run
+Large Scale: Design-Specify-Implement
+Building Your First Web Application
+The HTML5 Canvas
+JavaScript in a Separate File
+What You Have Learned
+
+Chapter 2
+Game Programming Basics
