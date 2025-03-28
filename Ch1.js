58a7e31bc4d6758d127d79fa3eb5769f7ce08581
NEVO <albertoborgato9@gmail.com>
2025-03-28 12:22:57 +0100

    Create Ch1.js

---
+++ Ch1.js
@@ -0,0 +1,20 @@
+A Beginner’s Guide to Writing Minecraft Plugins in JavaScript
+Walter Higgins
+
+Contents
+Preface
+Introduction
+Part I Building a Modding Workbench
+Chapter 1 Running Your Own Minecraft Server
+Client-Server Networking
+Why Run Your Own Server?
+Which Minecraft Server Software Should I Use?
+CanaryMod
+Installing CanaryMod
+Agreeing to the End User License Agreement
+Configuring Your Server
+Basic Server Administration Commands
+Connecting to Your Server
+Achievement Unlocked!
+Other Server Options
+
