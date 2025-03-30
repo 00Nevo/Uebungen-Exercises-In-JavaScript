e21922e9be5084adaacc6ab71d3b9d5914178602
NEVO <albertoborgato9@gmail.com>
2025-03-31 00:45:56 +0200

    Create Ch2.js

---
+++ Ch2.js
@@ -0,0 +1,19 @@
+
+Building Blocks of a Game
+The Game World
+The Game Loop
+Building a Game Application in JavaScript
+The Structure of a Program
+Types of Applications
+Functions
+Syntax Diagrams
+Calling a Function
+Update and Draw
+Program Layout
+Comments
+Instructions vs. Lines
+Whitespace and Indentation
+What You Have Learned
+
+Chapter 3
+Creating a Game World
