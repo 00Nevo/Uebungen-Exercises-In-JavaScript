71e08739fb5849a4f3fe8d7eb3fbade07d416011
NEVO <albertoborgato9@gmail.com>
2025-04-09 18:26:45 +0200

    Create Ch1-Book.Of-JavaScript.js

---
+++ Ch1-Book.Of-JavaScript.js
@@ -0,0 +1,31 @@
+Book of Javascript
+
+1
+WELCOME TO JAVASCRIPT!
+
+Is JavaScript for You?
+Is This Book for You?
+The Goals of This Book
+What Can JavaScript Do?
+What Are the Alternatives to JavaScript?
+  CGI Scripting
+  The Need for Back-and-Forth Communication
+  Server Overload by Concurrent Access
+  Security Restrictions
+  VBScript
+  Java
+  Flash
+JavaScript’s Limitations
+  JavaScript Can’t Talk to Servers
+  JavaScript Can’t Create Graphics
+  JavaScript Works Differently in Different Browsers
+Getting Started
+Where JavaScript Goes on Your Web Pages
+Dealing with Older Browsers
+Your First JavaScript
+Summary
+Assignment
+
+2
+USING VARIABLES AND BUILT-IN FUNCTIONS TO UPDATE YOUR WEB PAGES AUTOMATICALLY
+
