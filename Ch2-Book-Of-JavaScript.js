8787ce52024809f2e59673bcd4db7b3dbc33c0c9
NEVO <albertoborgato9@gmail.com>
2025-04-13 15:40:51 +0200

    Create Ch2-Book-Of-JavaScript.js

---
+++ Ch2-Book-Of-JavaScript.js
@@ -0,0 +1,37 @@
+2
+U S I N G V A R I A B L E S A N D B U I L T - I N
+F U N C T I O N S T O U P D A T E Y O U R W E B
+P A G E S A U T O M A T I C A L L Y
+
+Variables Store Information
+  Syntax of Variables
+  Naming Variables
+  Arithmetic with Variables
+Write Here Right Now: Displaying Results
+  Line-by-Line Analysis of Figure 2-4
+Strings
+More About Functions
+  alert()
+  Line-by-Line Analysis of Figure 2-9
+  prompt()
+Parameters
+Writing the Date to Your Web Page
+  Built-in Date Functions
+  Date and Time Methods
+  Code for Writing the Date and Time
+  Line-by-Line Analysis of Figure 2-12
+  Getting the Date and Time
+  Making Minor Adjustments
+  Getting the String Right
+  What Are Those Other Functions?
+  JavaScript and HTML
+  Why document.write()?
+How the European Space Agency Writes the Date to Its Page
+Summary
+Assignment
+
+3
+G I V I N G T H E B R O W S E R S
+WHAT THEY WANT
+
+
